4042728b96eb7980e0c7d4a084307f396e0e1539
mehadishakil <mehadishakil469@gmail.com>
2024-11-28 23:16:00 +0600

    Initial Commit

---
+++ Theme/ThemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeApp.swift
+//  Theme
+//
+//  Created by Mehadi Hasan on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ThemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThemeTests/ThemeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeTests.swift
+//  ThemeTests
+//
+//  Created by Mehadi Hasan on 28/11/24.
+//
+
+import Testing
+@testable import Theme
+
+struct ThemeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
